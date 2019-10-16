28f3252f8d53c56a0cae815d9a70e0d97db39e69
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2019-10-16 14:32:18 -0500

    Finalizacion curso React

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import iconInformation from '../assets/static/icon-information.png'
 import iconPlay from '../assets/static/icon-play.png'
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/2866076/pexels-photo-2866076.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Img 1"/>
+        <img className="carousel-item__img" src={cover} alt={title}/>
         <div className="carousel-item__details">
             <div>
                 <img className="carousel-item__details-icon" src={iconInformation} alt="Icono información"/>
                 <img className="carousel-item__details-icon" src={iconPlay} alt="Icono play"/>
             </div>
-            <p className="carousel-item__details--title">Titulo descriptivo</p>
-            <p className="carousel-item__details--subtitle">2019 +16 114 minutos</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
         </div>
     </div>
 );
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number
+}
 
 export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,39 +8,47 @@ import Footer from '../components/Footer';
 import FooterLogo from '../components/FooterLogo';
 import FooterSocial from '../components/FooterSocial';
 import FooterMarca from '../components/FooterMarca';
+import useInitialState from '../hooks/useInitialState'
+
+const API = 'http://localhost:3000/initalState';
 
 const App = () => {
-    const [ videos, setVideos ] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3000/initalState')
-            .then(response => response.json())
-            .then(data => setVideos(data))
-    }, []);
-
+    const initialState = useInitialState(API);
     return (
         <div className="App">
             <Header />
-            <Categories title="Lo más popular">
-                <Carousel>
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
+            {initialState.mylist.length > 0 && 
+                <Categories title="Lo más popular">
+                    <Carousel>
+                        {initialState.mylist.map(item =>
+                            <CarouselItem key={item.id} {...item}/>
+                        )}
+                    </Carousel>
+                </Categories>
+            }
+
             <Categories title="Lo más nuevo">
                 <Carousel>
-                    <CarouselItem />
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
                 </Carousel>
             </Categories>
+
             <Categories title="Series">
                 <Carousel>
-                    <CarouselItem />
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item}/>
+                    )}
                 </Carousel>
             </Categories>
+
             <Categories title="Largometrajes">
                 <Carousel>
                     <CarouselItem />
                 </Carousel>
             </Categories>
+
             <Footer>
                 <FooterLogo />
                 <FooterSocial />
